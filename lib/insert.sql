55ec6562463a2e17c403e6fe630d426f2595ad96
Billy Ferrara <willfira717@gmail.com>
2020-07-07 14:58:33 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+insert into bears (name, age, gender, color, temperment, alive) values ('Mr.Chocolate', 15, 'M', 'brown', 'calm', 1);
+insert into bears (name, age, gender, color, temperment, alive) values ('Rowdy', 12, 'M', 'dark brown', 'rowdy', 1);
+insert into bears (name, age, gender, color, temperment, alive) values
